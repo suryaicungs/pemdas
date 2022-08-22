16500c049eb36418a328901972b332703e340cd4
suryaicungs <111480037+suryaicungs@users.noreply.github.com>
2022-08-22 11:59:01 +0700

    Add files via upload

---
+++ python_latihan.py
@@ -0,0 +1,15 @@
+nd=input("nama depan saya")
+nb=input("nama belakang saya")
+print("nama saya adalah", nd,nb )
+
+a=int(input("masukan nilai A: "))
+b=float(input("masukan nilai B: "))
+c=a+b
+
+print("A=", a)
+print("B", b)
+print(a,"+",b,"=",c)
+
+x=int(input("masukan suhu celcius: "))
+y=(9/5*x) + 32
+print(x,"celcius=" ,y, "fahrenheit")
